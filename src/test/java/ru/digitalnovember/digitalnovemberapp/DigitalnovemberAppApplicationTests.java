f313cd6012daa63633c214c2ce91ded39dcdc1ef
17750859 <Sopilov.N.V@sberbank.ru>
2019-11-29 22:29:37 +0300

    Initial commit

---
+++ src/test/java/ru/digitalnovember/digitalnovemberapp/DigitalnovemberAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.digitalnovember.digitalnovemberapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalnovemberAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
